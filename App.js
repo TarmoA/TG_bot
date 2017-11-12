d20442db87c5b912f583004969cf6946fcead5b5
Tarmo Asikainen <tare.asikainen@gmail.com>
2017-11-12 23:28:57 +0200

    modify logging, add rate limiting Added simple rate limiting with queues. Now puts the bot on a 60 second timeout if a 429 error is recieved.

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-const Slimbot = require('slimbot');
+const Slimbot = require("slimbot");
 const token = require("./token");
 const slimbot = new Slimbot(token.token);
 const HuhuGen = require("./huhu.js");
 const generator = new HuhuGen();
-const logToFile = require("./Logger.js");
-const botName = "HuhuBot"
+const Logger = require("./Logger.js");
+const botName = "HuhuBot";
 
 
 
@@ -14,31 +14,84 @@ function parseMsg(msg) {
 
     if (msg.match("/huhu(@"+botName+")?")) {
         return generator.generateHuhu();
-    } else return null;
+    } else {
+        return null;
+    }
 
 
 }
 
+//Makes the bot not respons to messages for a minute
+var onTimeout = false;
+function timeoutBot() {
+    onTimeout= true;
+    setTimeout(function(){ onTimeout = false}, 60000);
+}
 
-// Register listeners
-slimbot.on('message', message => {
-    logToFile(message);
-    //tsekkaa onko viesti vanha
-    if (message.date * 1000 < Date.now()- 60000 ) {
-        console.log("received old message");
+//Queues for rate limiting
+var privateMsgQ = [];
+var groupMsgQ = [];
+const privN = 0;
+const groupN = 1;
 
+function checkRateLimit(timestamp, type) {
+    if (type === privN) {
+        // 1s = 1000ms
+        //30 messages per second
+        const limit = 1000;
+        privateMsgQ.push(timestamp);
+        if (privateMsgQ.length > 30) {
+            var prev = privateMsgQ.shift();
+            var res = (timestamp - prev <= limit);
+            if (!res) Logger.logErr("private message ratelimit reached");
+            return res;
+        } else {
+            return true;
+        }
     } else {
-            const text = message.text
-        if (text != undefined && text.length != 0 && text[0] === '/') {
-            const response = parseMsg(text);
-            if (message.chat.type == "private") {
-                //30 messages per second
-            } else {
-                //20 messages per minute
-            }
-            if (response != undefined && response != null) slimbot.sendMessage(message.chat.id, response);
+        //1min = 60000ms
+        //20 messages per minute
+        const limit = 60000;
+        groupMsgQ.push(timestamp);
+        if (groupMsgQ.length > 20) {
+            var prev = privateMsgQ.shift();
+            var res = (timestamp - prev <= limit);
+            if (!res) Logger.logErr("group message limit reached");
+            return res;
+        } else {
+            return true;
         }
     }
+}
+
+
+
+// Register listeners
+slimbot.on("message", message => {
+    if (onTimeout) return;
+    var responseSent = false;
+    //Don't respond to messages older than a minute
+    if (message.date * 1000 < Date.now()- 60000 ) {
+        Logger.logErr("received old message");
+
+    } else {
+            const chatType = (message.chat.type === "private" ? privN : groupN);
+            const text = message.text
+        if (text && text.length !== 0 && text[0] === "/" && checkRateLimit(message.date, chatType) ) {
+            const response = parseMsg(text);
+            if (response) {
+                slimbot.sendMessage(message.chat.id, response, function(err, msg){
+                    if (err) {
+                        Logger.logErr("got 429 error");
+                        //timeout the bot for 60 seconds
+                        timeoutBot();
+                    }
+                });
+                responseSent = true;
+            }
+        }
+    }
+    Logger.logMsg(message, responseSent);
 });
 
 // Call API
--- Logger.js
+++ Logger.js
@@ -2,12 +2,27 @@ const fs = require('fs');
 
 
 var logFile = fs.createWriteStream("./log.txt", {flags: 'a'});
+var errFile = fs.createWriteStream("./errlog.txt", {flags: 'a'});
 
-function logToFile(message) {
-    const date = message.date != undefined ? message.date : "-1"
-    const from = message.from != undefined ? message.from.id : "???"
-    const text = message.text != undefined ? message.text : "-"
-    logFile.write(date +" " + from + " " + text+ "\n");
+function logToFile(message, responseSent) {
+    const date = message.date !== undefined ? new Date(message.date*1000).toLocaleString() : "-1";
+    const user = message.from;
+    var userStr = "";
+    if (user !== undefined) {
+        userStr += user.id;
+    }
+    const chatType = message.chat !== undefined ? message.chat.type : "-";
+    const text = message.text !== undefined ? message.text : "-";
+    logFile.write(date + " " + userStr + " " + chatType + " " + text+ " " + responseSent + "\n");
 }
 
-module.exports = logToFile;
+function logError(msg) {
+    errFile.write(msg + "\n");
+}
+
+
+
+module.exports = {
+    logMsg: logToFile,
+    logErr: logError
+}
